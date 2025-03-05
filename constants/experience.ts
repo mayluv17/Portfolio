07e55901423bd7b17a7fe93500ab8fcb0c993107
ibrahim abdulganiy <mayowaibrahim009@gmail.com>
2025-03-05 13:05:38 +0200

    fix location

--- constants/experience.ts
+++ constants/experience.ts
@@ -4,7 +4,7 @@ export const experiences = [
     duration: 'June 2021 - Jan 2023',
     link: '#',
     company: 'Xircuitron ',
-    location: 'https://flagcdn.com/w320/us.png',
+    location: '',
     description1:
       'Developed modern, responsive and dynamic user interfaces of a robust management Software-as-a-Service (SaaS) solution, adopted by over 200 educational institutions, leading to a remarkable 100% increase in teacher productivity and the eradication of paperwork.',
     technologies: [
@@ -23,7 +23,7 @@ export const experiences = [
     duration: 'Jan 2020 - June 2022',
     link: '#',
     company: '@TopChoice Digitals ',
-    location: 'https://flagcdn.com/w320/gb.png',
+    location: '',
     description1:
       'Transformed an established logistic and delivery tracking portal, serving a vast user base of 5000+ monthly users, into an intuitively responsive application, amplifying the overall user experience and ensuring seamless usability across mobile and tablet devices.',
     technologies: [
@@ -40,8 +40,8 @@ export const experiences = [
     role: 'Fullstack Developer Intern',
     duration: 'July 2021 - Dec 2021',
     link: 'https://wildfire.ng/',
-    company: '@WildFire ',
-    location: 'https://flagcdn.com/w320/ng.png',
+    company: '@Pluxnet ',
+    location: '',
     description1:
       'Masterminded efforts focused on utilizing Facebook API for integrating publishing services as well as implemented Single Sign-In using Facebook, Google, and Twitter. Improve SEO, resulting in increased website visibility, higher organic search rankings, and improved user engagement.',
     technologies: ['PHP', 'NodeJs', 'Javascript', 'MySql'],
